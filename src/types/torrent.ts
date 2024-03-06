9fa2389b411db0b40bb3389ef5ff7857ab79489c
Sophia (Turner) <me@sophia-dev.io>
2024-03-06 21:47:47 +0900

    refactor(torrent): replace torrent object into class instead of pure object

--- src/types/torrent.ts
+++ src/types/torrent.ts
@@ -1,6 +1,7 @@
 import { TorrentState } from '../enums'
+import { Client } from '../client'
 
-export interface Torrent {
+export interface RawTorrent {
   dlspeed: number
   eta: number
   f_l_piece_prio: boolean
@@ -21,4 +22,34 @@ export interface Torrent {
   state: TorrentState
   super_seeding: boolean
   upspeed: number
+}
+
+export class Torrent implements RawTorrent {
+  public readonly dlspeed!: number
+  public readonly eta!: number
+  public readonly f_l_piece_prio!: boolean
+  public readonly force_start!: boolean
+  public readonly hash!: string
+  public readonly category!: string
+  public readonly tags!: string
+  public readonly name!: string
+  public readonly num_complete!: number
+  public readonly num_incomplete!: number
+  public readonly num_leechs!: number
+  public readonly num_seeds!: number
+  public readonly priority!: number
+  public readonly progress!: number
+  public readonly ratio!: number
+  public readonly seq_dl!: boolean
+  public readonly size!: number
+  public readonly state!: TorrentState
+  public readonly super_seeding!: boolean
+  public readonly upspeed!: number
+
+  private readonly client: Client
+
+  public constructor(client: Client, data: RawTorrent) {
+    Object.assign(this, data)
+    this.client = client
+  }
 }
