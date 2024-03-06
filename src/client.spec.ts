617df9b1aa3fcc8d52779aad65217736d22515ab
Sophia (Turner) <me@sophia-dev.io>
2024-03-06 16:03:37 +0900

    refactor(client): enhance client login logic with correct body and failure case

--- src/client.spec.ts
+++ src/client.spec.ts
@@ -4,21 +4,50 @@ import { Client } from './client'
 describe('Client', () => {
   it('should be able to login', async () => {
     nock('http://localhost:8080')
-      .post('/api/v2/auth/login', { username: 'user', password: 'pass' })
-      .reply(200, '', { SID: '123' })
+      .post('/api/v2/auth/login')
+      .query(true)
+      .reply(200, '', { 'set-cookie': 'SID=1234' })
 
     await expect(
       Client.login('http://localhost:8080', 'user', 'pass'),
-    ).resolves.not.toThrow()
+    ).resolves.toMatchObject(
+      expect.objectContaining({
+        host: 'http://localhost:8080',
+        sessionId: expect.stringMatching('1234'),
+      }),
+    )
   })
 
   it('should throw an error when login fails', async () => {
     nock('http://localhost:8080')
-      .post('/api/v2/auth/login', { username: 'user', password: 'pass' })
+      .post('/api/v2/auth/login')
+      .query(true)
       .reply(401)
 
     await expect(
       Client.login('http://localhost:8080', 'user', 'pass'),
     ).rejects.toThrow('Login failed')
+  })
+
+  it('should throw an error when no cookies are returned', async () => {
+    nock('http://localhost:8080')
+      .post('/api/v2/auth/login')
+      .query(true)
+      .reply(200)
+
+    await expect(
+      Client.login('http://localhost:8080', 'user', 'pass'),
+    ).rejects.toThrow('Failed to get cookies from response')
+  })
+
+  it('should throw an error when no sessionId is returned', async () => {
+    nock('http://localhost:8080')
+      .post('/api/v2/auth/login')
+      .query(true)
+      .reply(200, '', { 'set-cookie': 'SID=' })
+
+    await expect(
+      Client.login('http://localhost:8080', 'user', 'pass'),
+    ).rejects.toThrow('Failed to get sessionId from response')
   })
 })
--- src/client.ts
+++ src/client.ts
@@ -1,17 +1,28 @@
+import setCookie from 'set-cookie-parser'
+import fetch from 'node-fetch'
+
 export class Client {
   public static async login(host: string, username: string, password: string) {
     const response = await fetch(`${host}/api/v2/auth/login`, {
       method: 'POST',
-      body: JSON.stringify({ username, password }),
+      body: new URLSearchParams({ username, password }),
     })
 
     if (!response.ok) {
       throw new Error('Login failed')
     }
 
-    const sessionId = response.headers.get('SID')
+    const cookies = response.headers.get('set-cookie')
+    if (!cookies) {
+      throw new Error('Failed to get cookies from response')
+    }
+
+    const sessionId = setCookie
+      .parse(cookies)
+      .find((cookie) => cookie.name === 'SID')?.value
+
     if (!sessionId) {
-      throw new Error('Login failed')
+      throw new Error('Failed to get sessionId from response')
     }
 
     return new Client(host, sessionId)
